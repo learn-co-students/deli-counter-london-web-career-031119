ba04056c4fd6fef5736afe4b3cd4b289c0b1fabf
deeseert <gio.amms@gmail.com>
2019-02-16 11:46:43 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+katz_deli = []
+
+def line(linee)
+  if !linee.length
+    return "The line is currently empty."
+  else
+    line.each do |i|
+      return "The line is currently: #{i}. #{linee[i]}"
+  end
+  # katz_deli.each do |i|
+  #   if katz_deli == 0
+  #     puts "The line is currently empty."
+  #   end
+  #   puts "The line is currently: #{index}. #{name}"
+  # end
+end
