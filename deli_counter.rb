cfb178af351d12a23f1a0d251ff0b95c569025fc
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-25 22:49:57 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def line(line) 
+  if line.emty?
+    puts "the line is Currently emty." 
+ end
+def line(line) 
+  if line.emty?
+    puts "the line is Currently emty." 
+   else 
+      current_line = "The line is currently:"
+    line.each.with_index(1) do |name, i|
+      current_line << " #{i}. #{name}"
+    end
+    puts current_line
+    
+end
+def take_a_number(name,line)
+  name.push
+  puts "Welcome, #{name}.You are number #{line.length} in line"
+end
+def nos_serving(line)
+  if line.length == 0 
+  if line.emty?
+    puts "There is nobody waiting to be served!"
+  else
+  puts "Currently serving #{line.first}."
+  line.shift
+end
